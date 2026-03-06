d99baed5ad9f2cb5830b59e8d5a76a4f7c693d51
Alan Marcero <alanmarcero@gmail.com>
2026-03-06 10:32:42 -0500

    Schedule scanner hourly Wed-Fri 10am-3pm + 4:05pm post-close

--- aws-scanner/terraform/eventbridge.tf
+++ aws-scanner/terraform/eventbridge.tf
@@ -1,8 +1,8 @@
-resource "aws_scheduler_schedule" "intraday" {
-  name       = "ema-scanner-intraday"
+resource "aws_scheduler_schedule" "hourly" {
+  name       = "ema-scanner-hourly"
   group_name = "default"
 
-  schedule_expression          = "cron(0 14 ? * WED-FRI *)"
+  schedule_expression          = "cron(0 10-15 ? * WED-FRI *)"
   schedule_expression_timezone = "America/New_York"
 
   flexible_time_window {
@@ -19,7 +19,7 @@ resource "aws_scheduler_schedule" "post_close" {
   name       = "ema-scanner-post-close"
   group_name = "default"
 
-  schedule_expression          = "cron(5 16 ? * FRI *)"
+  schedule_expression          = "cron(5 16 ? * WED-FRI *)"
   schedule_expression_timezone = "America/New_York"
 
   flexible_time_window {
